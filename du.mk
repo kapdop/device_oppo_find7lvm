f83f2834c08530b80ebbc04b5573280787e16eb9
chasmodo <bosko.trkulja@gmail.com>
2015-03-28 20:10:16 +0100

    Cleanup and small fixes
    Change-Id: I9a05312ba654ac48124b5b18c6752a0c81f641d8

--- du.mk
+++ du.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/oppo/find7lvm/full_find7lvm.mk)
 
 ## Enhanced NFC
-#$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 #
-## Inherit some common stuff.
+# Inherit some common stuff.
 #$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 PRODUCT_NAME := du_find7lvm
